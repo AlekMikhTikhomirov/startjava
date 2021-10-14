2dc4ca432a089dbe98c5baa4f48445f7a855d0a4
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-10-15 01:54:03 +0300

    frames was deleted for abcdef systemout

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -6,11 +6,11 @@ public class Variable {
         long d=2300000000L;
         float e=2.3E+9f;
         double f=0.0023E+12d;
-    System.out.println ("Количество ядер процессора:"+" "+"a");
-System.out.println ("Ёмкость ПЗУ в гигабайтах:"+" "+"b");
-System.out.println ("Ёмкость ОЗУ в мегабайтах:"+" "+"с");
-System.out.println ("Тактовая частота процессора в герцах:"+" "+"d");
-System.out.println ("Она же в экспоненциальном виде:"+" "+"e");
-System.out.println ("Она же в экспоненциальном виде в килогерцах"+" "+"f");
+    System.out.println ("Количество ядер процессора:"+" "+a);
+System.out.println ("Ёмкость ПЗУ в гигабайтах:"+" "+b);
+System.out.println ("Ёмкость ОЗУ в мегабайтах:"+" "+с);
+System.out.println ("Тактовая частота процессора в герцах:"+" "+d);
+System.out.println ("Она же в экспоненциальном виде:"+" "+e);
+System.out.println ("Она же в экспоненциальном виде в килогерцах"+" "+f);
     }
 }
