7398f0f11d359c225bc1c24b43854d8c844b8b5a
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-10-15 01:44:39 +0300

    L was added at the end of long number

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -3,7 +3,7 @@ public class Variable {
         byte a=2;
         short b=320;
         int c=6000;
-        long d=2300000000;
+        long d=2300000000L;
         float e=2.3E+9f;
         double f=0.0023E+12d;
     System.out.println ("Количество ядер процессора:"+" "+"a");
