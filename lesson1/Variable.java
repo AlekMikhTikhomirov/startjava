bac931936e8d8492f43abe704f8310d581aeccf5
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-10-15 01:31:18 +0300

    error of large integer number was fixed

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,16 +1,16 @@
 public class Variable {
     public static void main(String[] args) {
         byte a=2;
-        short b=6000;
-        integer c=6000000;
+        short b=320;
+        int c=6000;
         long d=2300000000;
         float e=2.3E+9f;
         double f=0.0023E+12d;
     System.out.println ("Количество ядер процессора:"+" "+"a");
-System.out.println ("Ёмкость ОЗУ в мегабайтах:"+" "+"b");
-System.out.println ("Ёмкость ОЗУ в килобайтах:"+" "+"с");
+System.out.println ("Ёмкость ПЗУ в гигабайтах:"+" "+"b");
+System.out.println ("Ёмкость ОЗУ в мегабайтах:"+" "+"с");
 System.out.println ("Тактовая частота процессора в герцах:"+" "+"d");
-System.out.println ("Тактовая частота процессора в герцах:"+" "+"e");
-System.out.println ("Тактовая частота процессора в килогерцах"+" "+"f");
+System.out.println ("Она же в экспоненциальном виде:"+" "+"e");
+System.out.println ("Она же в экспоненциальном виде в килогерцах"+" "+"f");
     }
 }
