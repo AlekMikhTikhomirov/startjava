421ec35277004dd066e81403bd6f14a371f64f34
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-10-15 14:49:48 +0300

    final version of Variable was added to index

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,16 +1,16 @@
 public class Variable {
-    byte a=2;
-    short b=320;
-    int c=6000;
-    long d=2300000000L;
-    float e=2.3E+9f;
-    double f=0.0023E+12d;
-    public static void main(String[] args) {
-        System.out.println ("Количество ядер процессора:"+" "+a);
-System.out.println ("Ёмкость ПЗУ в гигабайтах:"+" "+b);
-System.out.println ("Ёмкость ОЗУ в мегабайтах:"+" "+с);
-System.out.println ("Тактовая частота процессора в герцах:"+" "+d);
-System.out.println ("Она же в экспоненциальном виде:"+" "+e);
-System.out.println ("Она же в экспоненциальном виде в килогерцах:"+" "+f);
+    public static void main (String[] args) {
+        byte a=2;
+        short b=320;
+        int c=6000;
+        long d=2300000000L;
+        float e=2.3E+9f;
+        double f=6.022E+23d;
+        System.out.println("Количество ядер процессора:"+" "+ a);
+        System.out.println("Ёмкость ПЗУ в гигабайтах:"+" "+ b);
+        System.out.println("Ёмкоcть ОЗУ в мегабайтах:"+" "+ c);
+        System.out.println("Тактовая частота процессора в герцах:"+" "+ d);
+        System.out.println("Она же в экспоненциальном виде:"+" "+ e);
+        System.out.println("А это просто число Авогадро:"+" "+ f);
     }
 }
