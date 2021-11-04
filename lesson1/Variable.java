f8175af261136250c906638391cddc9bcd55248d
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-11-04 11:30:05 +0300

    mistakes in MyFirstApp & Variable are fixed

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -1,16 +1,16 @@
 public class Variable {
-    public static void main (String[] args) {
-        byte a=2;
-        short b=320;
-        int c=6000;
-        long d=2300000000L;
-        float e=2.3E+9f;
-        double f=6.022E+23d;
-        System.out.println("Количество ядер процессора:"+" "+ a);
-        System.out.println("Ёмкость ПЗУ в гигабайтах:"+" "+ b);
-        System.out.println("Ёмкоcть ОЗУ в мегабайтах:"+" "+ c);
-        System.out.println("Тактовая частота процессора в герцах:"+" "+ d);
-        System.out.println("Она же в экспоненциальном виде:"+" "+ e);
-        System.out.println("А это просто число Авогадро:"+" "+ f);
+    public static void main(String[] args) {
+        byte cores = 2;
+        short PZU = 320;
+        int OZU = 6000;
+        long rate = 2300000000L;
+        float exprate = 2.3E+9f;
+        double Avogadro = 6.022E+23d;
+        System.out.println("Количество ядер процессора:" + " " + cores);
+        System.out.println("Ёмкость ПЗУ в гигабайтах:" + " " + PZU);
+        System.out.println("Ёмкоcть ОЗУ в мегабайтах:" + " " + OZU);
+        System.out.println("Тактовая частота процессора в герцах:" + " " + rate);
+        System.out.println("Она же в экспоненциальном виде:" + " " + exprate);
+        System.out.println("А это просто число Авогадро:" + " " + Avogadro);
     }
 }
