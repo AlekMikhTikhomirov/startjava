4ffa6d5b618138c1387b0db319df79e41fd861c8
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-12-01 17:50:57 +0300

    catalog lesson2 was added to repo

---
+++ lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "Male";
+    String name = "Aleksei";
+    float height = 1.83f;
+    float weight = 78.7f;
+    int age = 38;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    void seat() {
+        System.out.println("sitting");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    String talk() {
+        return "talking";
+    }
+
+    void studyJava() {
+        System.out.println("studying Java");
+    }
+}
