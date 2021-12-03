cdb3afcaaf8c18b8af0bf76b859a44ec054ceb2a
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-12-04 00:31:07 +0300

    WolfTest was fixed

--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        String gender = wolfOne.gender;
-        String nickname = wolfOne.nickname;
-        float weight = wolfOne.weight;
-        int age = wolfOne.age;
-        String color = wolfOne.color;
-        System.out.println("Пол:" + gender);
-        System.out.println("Позывной:"+ nickname);
-        System.out.println("Вес:"+ weight);
-        System.out.println("Возраст:"+ age);
-        System.out.println("Цвет:"+ color);
+        String gender = "Female";
+        String nickname = "Yara";
+        float weight = 0.6f;
+        int age = 4;
+        String color = "Black";
+        System.out.println("Пол:" + wolfOne.gender);
+        System.out.println("Позывной:" + wolfOne.nickname);
+        System.out.println("Вес:" + wolfOne.weight);
+        System.out.println("Возраст:" + wolfOne.age);
+        System.out.println("Цвет:" + wolfOne.color);
         wolfOne.walk();
         wolfOne.hunt();
         wolfOne.seat();
