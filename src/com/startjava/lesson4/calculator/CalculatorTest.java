0334d3c388d0674beed201e7e3fffcb8b9b3daf9
AlekMikhTikhomirov <AlekMikh4home@gmail.com>
2021-12-27 01:53:03 +0300

    calculator of lesson 4 was fixed

--- src/com/startjava/lesson4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson4/calculator/CalculatorTest.java
@@ -4,25 +4,23 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculatorOne = new Calculator();
+        Calculator calculator = new Calculator();
         Scanner console = new Scanner(System.in);
         String chooseYesNo;
         do {
             System.out.println("Введите строку в формате: [первое число, пробел, знак математической операции, пробел, второе число (без запятых)]");
-            calculatorOne.setMathExpression(console.nextLine());
-            calculatorOne.calculate();
+            calculator.setMathExpression(console.nextLine());
+            calculator.calculate();
             System.out.println("Хотите продолжить вычисления? [yes / no]");
             do {
                 chooseYesNo = console.nextLine();
                 if (chooseYesNo.equals("yes")) {
                     System.out.println("Продолжим!");
                     break;
-                } else if (chooseYesNo.equals("no")) {
-                    break;
                 } else {
                     System.out.println("Введите yes или no");
                 }
-            } while (chooseYesNo != "no");
+            } while (!chooseYesNo.equals("no"));
         } while (chooseYesNo.equals("yes"));
         System.out.println("До свидания!");
     }
